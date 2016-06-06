a45d1896fe988bf95bfd2ebeb21d003f9e088aba
Josiah Baldwin <josiah.baldwin@intel.com>
2016-06-06 16:58:55 -0700

    Fixed xordiff algorithm to remove useless xoring. Whitespace stuff

--- src/xordiff/Cython/xordiff.pyx
+++ src/xordiff/Cython/xordiff.pyx
@@ -6,47 +6,41 @@ cimport cython
 def numpyxor(np.ndarray[np.uint8_t, ndim=1] n1, np.ndarray[np.uint8_t, ndim=1] n2, int size):
     cdef np.ndarray[np.uint8_t, ndim=1] h = np.empty(size, dtype=np.uint8)
     cdef np.ndarray[np.uint_t, ndim=1] retoff
-    cdef int i = 0
-    cdef int j = 0
-    cdef char state = 0
-    cdef char xor = 0
-    cdef int off = 0
-    cdef int length = 0
+    cdef int i = 0, j = 0, state = 0
     offsets = [0]
-    
+
     while i < size:
             if state == 0:
-                xor = n1[<unsigned int>i] ^ n2[<unsigned int>i]
-                h[<unsigned int>j] = xor
-                if xor == 0:
+                if n1[<unsigned int>i] == n2[<unsigned int>i]:
+                    offsets.append(i)
                     state = 1
-                j += 1
-                
+                else:
+                    h[<unsigned int>j] = n1[<unsigned int>i]
+                    j += 1
+
             elif state == 1:
-                xor = n1[<unsigned int>i] ^ n2[<unsigned int>i]
-                if xor != 0:
-                    h[j] = xor
+                if n1[<unsigned int>i] != n2[<unsigned int>i]:
+                    h[<unsigned int>j] = n1[<unsigned int>i]
                     j += 1
                     offsets.append(i)
                     state = 0
             i+=1
+    if state == 0:
+        offsets.append(i)
     retoff = np.array(offsets,dtype=np.uint)
     h.resize(j, refcheck=False)
     return h,retoff
-        
+
 @cython.boundscheck(False)
 def numpyundif(np.ndarray[np.uint8_t, ndim=1] n1, np.ndarray[np.uint8_t, ndim=1] n2, np.ndarray[np.uint_t, ndim=1] offsets):
     cdef np.ndarray[np.uint8_t, ndim=1] retarr = n2.copy()
-    cdef int i = 0
-    cdef int n1off = 0
-    cdef int off = 0
-    while i < n1.size:
-        if n1[<unsigned int>i] == 0:
-            off += 1
-            n1off = 0
-        else:
-            retarr[<unsigned int>(offsets[<unsigned int>off]+n1off)] ^= n1[<unsigned int>i]
+    cdef int i = 0, j = 0, n1off = 0, start = 0, stop = 0
+    while i < offsets.size:
+        off = start = offsets[i]
+        stop = offsets[i+1]
+        while j < stop:
+            retarr[<unsigned int>j] = n1[<unsigned int>n1off]
             n1off += 1
-        i += 1
-    
+            j += 1
+        i += 2
     return retarr
--- src/xordiff/xordiffnumpy.py
+++ src/xordiff/xordiffnumpy.py
@@ -6,6 +6,6 @@ class numpyxor(object):
         self.diffarr, self.offsets = _xordiff.numpyxor(n1,n2,n1.size)
         self.size = self.diffarr.size + self.offsets.size
         self.nbytes = self.diffarr.nbytes + self.offsets.nbytes
-        
+
     def undiff(self,n2):
         return _xordiff.numpyundif(self.diffarr,n2,self.offsets)
